155a01e5c53a472b74c953318f9a6d97705d727b
minimum2106 <tranhongquan2106@gmail.com>
2025-04-02 11:54:03 +0200

    add method __getitem__ and fix the regex pattern for loading data

--- custom_mnist.py
+++ custom_mnist.py
@@ -22,17 +22,34 @@ class CustomMNIST(VisionDataset):
     def __init__(self, root, transforms = None, transform = None, target_transform = None):
         super().__init__(root, transforms, transform, target_transform)
 
-        self.images, self.labels = self.__load_data()
+        self.images, self.labels, self.filenames = self.__load_data()
         self.train = True
 
     def __load_data(self):
         images = []
-        for f in glob.iglob(f"{self.root}/images"):
+        filenames = glob.iglob(f"{self.root}/images/*.jpg")
+        for f in filenames:
             images.append(np.asarray(Image.open(f)))
         labels = np.load(f"{self.root}/labels")
 
-        return images, labels
+        return images, labels, filenames
 
     def __len__(self):
         return len(self.images)
+    
+    def __getitem__(self, index):
+        img, target, filename = self.images[index], int(self.labels[index]), self.filenames[index]
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img.numpy(), mode="L")
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target, filename
+
 
